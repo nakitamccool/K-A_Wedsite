6d1963c0dc5ea99a458f6b3900927baee710589d
nakitamccool <nakitamccool@gmail.com>
2017-05-12 11:11:00 +0100

    updated invite section, added section titles

--- js/maps.js
+++ js/maps.js
@@ -1,17 +1,114 @@
 /**
  * Created by nakita on 05/05/2017.
  */
+
 function initMap() {
     //Location coordinates
     var church = {lat: 51.910518, lng: -8.170365};
     var reception = {lat: 51.911443, lng: -8.063080};
     var bnb = {lat: 51.921212, lng: -8.084154};
-    var dublinairport = {lat: 53.426334, lng: -6.249013};
-    var corkairport = {lat: 51.844726, lng: -8.491990};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10,
-        center: church
-    });
+    var mapOptions = {
+        zoom: 11,
+        center: church,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        panControl: false,
+        zoomControl: true,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
+
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.TOP_LEFT
+        },
+
+        scaleControl: true,
+        scaleControlOptions: {
+            position: google.maps.ControlPosition.TOP_CENTER
+        },
+        streetViewControl: false,
+        overviewMapControl: false,
+
+        styles: [{
+            "stylers": [{
+                "saturation": -100
+            }, {
+                "gamma": 1
+            }]
+        }, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi.business",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi.business",
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi.place_of_worship",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "poi.place_of_worship",
+            "elementType": "labels.icon",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+                "visibility": "simplified"
+            }]
+        }, {
+            "featureType": "water",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "saturation": 50
+            }, {
+                "gamma": 0
+            }, {
+                "hue": "#50a5d1"
+            }]
+        }, {
+            "featureType": "administrative.neighborhood",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#333333"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "labels.text",
+            "stylers": [{
+                "weight": 0.5
+            }, {
+                "color": "#333333"
+            }]
+        }, {
+            "featureType": "transit.station",
+            "elementType": "labels.icon",
+            "stylers": [{
+                "gamma": 1
+            }, {
+                "saturation": 50
+            }]
+        }]
+    };
+
+    var map = new google.maps.Map(document.getElementById('map'),mapOptions);
 
     //Church marker and pop up box
     var churchPopUp = '<div id="content">' +
@@ -31,7 +128,8 @@ function initMap() {
     var churchmarker = new google.maps.Marker({
         position: church,
         title: 'Wedding ceremony',
-        map: map
+        map: map,
+        icon: "img/mapmarkers/church.png"
     });
     churchmarker.addListener('click', function () {
         churchinfowindow.open(map, churchmarker);
@@ -55,7 +153,8 @@ function initMap() {
     var receptionmarker = new google.maps.Marker({
         position: reception,
         title: 'Wedding reception',
-        map: map
+        map: map,
+        icon: "img/mapmarkers/hotel.png"
     });
     receptionmarker.addListener('click', function () {
         receptioninfowindow.open(map, receptionmarker);
@@ -84,49 +183,4 @@ function initMap() {
         bnbinfowindow.open(map, bnbmarker);
     });
 
-    //Airport markers and pop up boxes
-
-    var dublinairportPopUp = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h3 id="firstHeading" class="firstHeading">Dublin Airport</h3>' +
-        '<div id="bodyContent">' +
-        '<p><i class="fa fa-map-marker"></i><span class="mapdetail">Dublin airport, Swords, Co. Dublin, K67 X2V0</span></p>' +
-        '<p><a href="https://www.dublinairport.com/">' + '<i class="fa fa-globe"><span class="mapdetail">www.dublinairport.com</span></i></a></p>' +
-        '<p><a href="tel:018141111"><i class="fa fa-phone"><span class="mapdetail">(01) 814 1111</span></i></a></p>' +
-        '</div>' +
-        '</div>';
-    var dublinairportinfowindow = new google.maps.InfoWindow({
-        content: dublinairportPopUp
-    });
-    var dublinairportmarker = new google.maps.Marker({
-        position: dublinairport,
-        title: 'Dublin airport',
-        map: map
-    });
-    dublinairportmarker.addListener('click', function () {
-        dublinairportinfowindow.open(map, dublinairportmarker);
-    });
-
-    var corkairportPopUp = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h3 id="firstHeading" class="firstHeading">Cork Airport</h3>' +
-        '<div id="bodyContent">' +
-        '<p><i class="fa fa-map-marker"></i><span class="mapdetail">Cork airport, Kinsale Road, Co. Cork, T12 YH66</span></p>' +
-        '<p><a href="https://www.corkairport.com/">' + '<i class="fa fa-globe"><span class="mapdetail">www.corkairport.com</span></i></a></p>' +
-        '<p><a href="tel:0214313131"><i class="fa fa-phone"><span class="mapdetail">(021) 431 3131</span></i></a></p>' +
-        '</div>' +
-        '</div>';
-    var corkairportinfowindow = new google.maps.InfoWindow({
-        content: corkairportPopUp
-    });
-    var corkairportmarker = new google.maps.Marker({
-        position: corkairport,
-        title: 'Cork airport',
-        map: map
-    });
-    corkairportmarker.addListener('click', function () {
-        corkairportinfowindow.open(map, corkairportmarker);
-    });
 }
